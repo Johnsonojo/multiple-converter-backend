e31f564abd138884bec624c28c1a8bd92684ab29
Johnsonojo <johnsonojo89@gmail.com>
2020-11-21 20:19:47 +0100

    add mass conversion functionality

---
+++ src/tests/__mock__/massData.js
@@ -0,0 +1,44 @@
+const typeAndValue = {
+  tonne: {
+    massValue: 40,
+    massType: "Tonne",
+  },
+  kilogram: {
+    massValue: 40,
+    massType: "Kilogram",
+  },
+  gram: {
+    massValue: 40,
+    massType: "Gram",
+  },
+  milligram: {
+    massValue: 40,
+    massType: "Milligram",
+  },
+  microgram: {
+    massValue: 40,
+    massType: "Microgram",
+  },
+  imperialTon: {
+    massValue: 40,
+    massType: "Imperial Ton",
+  },
+  usTon: {
+    massValue: 40,
+    massType: "Us Ton",
+  },
+  stone: {
+    massValue: 40,
+    massType: "Stone",
+  },
+  pound: {
+    massValue: 40,
+    massType: "Pound",
+  },
+  ounce: {
+    massValue: 40,
+    massType: "Ounce",
+  },
+};
+
+export default typeAndValue;
---
+++ src/tests/mass.spec.js
@@ -0,0 +1,94 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+import app from "../index";
+import typeAndValue from "./__mock__/massData";
+
+const { tonne, kilogram } = typeAndValue;
+
+let req;
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe("Mass Controller", () => {
+  before(async () => {
+    req = chai.request(app).keepOpen();
+  });
+
+  after(async () => {
+    req.close();
+  });
+
+  describe("Tonne Conversion", () => {
+    it("should convert tonne mass values", async () => {
+      const res = await req.post("/api/v1/mass").send(tonne);
+      expect(res.status).to.equal(200);
+      expect(res.body.status).to.equal("success");
+      expect(res.body).to.be.an("object");
+      expect(res.body.convertedMass).to.be.an("object");
+
+      expect(res.body.convertedMass).to.have.property("valueInKilograms");
+      expect(res.body.convertedMass.valueInKilograms).to.equal("4.0000e+4");
+
+      expect(res.body.convertedMass).to.have.property("valueInGrams");
+      expect(res.body.convertedMass.valueInGrams).to.equal("4.0000e+7");
+
+      expect(res.body.convertedMass).to.have.property("valueInMilligrams");
+      expect(res.body.convertedMass.valueInMilligrams).to.equal("4.0000e+10");
+
+      expect(res.body.convertedMass).to.have.property("valueInMicrograms");
+      expect(res.body.convertedMass.valueInMicrograms).to.equal("4.0000e+13");
+
+      expect(res.body.convertedMass).to.have.property("valueInImperialTon");
+      expect(res.body.convertedMass.valueInImperialTon).to.equal("3.9370e+1");
+
+      expect(res.body.convertedMass).to.have.property("valueInUsTon");
+      expect(res.body.convertedMass.valueInUsTon).to.equal(44.080000000000005);
+
+      expect(res.body.convertedMass).to.have.property("valueInStone");
+      expect(res.body.convertedMass.valueInStone).to.equal("6.2989e+3");
+
+      expect(res.body.convertedMass).to.have.property("valueInPounds");
+      expect(res.body.convertedMass.valueInPounds).to.equal(88184.79999999999);
+
+      expect(res.body.convertedMass).to.have.property("valueInOunce");
+      expect(res.body.convertedMass.valueInOunce).to.equal("1.4110e+6");
+    });
+  });
+
+  describe("Kilogram Conversion", () => {
+    it("should convert kilogram mass values", async () => {
+      const res = await req.post("/api/v1/mass").send(kilogram);
+      expect(res.status).to.equal(200);
+      expect(res.body.status).to.equal("success");
+      expect(res.body).to.be.an("object");
+      expect(res.body.convertedMass).to.be.an("object");
+
+      expect(res.body.convertedMass).to.have.property("valueInTonnes");
+      expect(res.body.convertedMass.valueInTonnes).to.equal(0.04);
+
+      expect(res.body.convertedMass).to.have.property("valueInGrams");
+      expect(res.body.convertedMass.valueInGrams).to.equal("4.0000e+4");
+
+      expect(res.body.convertedMass).to.have.property("valueInMilligrams");
+      expect(res.body.convertedMass.valueInMilligrams).to.equal("4.0000e+7");
+
+      expect(res.body.convertedMass).to.have.property("valueInMicrograms");
+      expect(res.body.convertedMass.valueInMicrograms).to.equal("4.0000e+10");
+
+      expect(res.body.convertedMass).to.have.property("valueInImperialTon");
+      expect(res.body.convertedMass.valueInImperialTon).to.equal("3.9370e-2");
+
+      expect(res.body.convertedMass).to.have.property("valueInUsTon");
+      expect(res.body.convertedMass.valueInUsTon).to.equal("4.4101e-2");
+
+      expect(res.body.convertedMass).to.have.property("valueInStone");
+      expect(res.body.convertedMass.valueInStone).to.equal("6.2992e+0");
+
+      expect(res.body.convertedMass).to.have.property("valueInPounds");
+      expect(res.body.convertedMass.valueInPounds).to.equal(88.1848);
+
+      expect(res.body.convertedMass).to.have.property("valueInOunce");
+      expect(res.body.convertedMass.valueInOunce).to.equal("1.4110e+3");
+    });
+  });
+});
